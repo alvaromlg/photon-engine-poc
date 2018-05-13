5b87533ebf2eb2aedc92d5e754171d9d0c9ab552
Alvaro <apelegrinafernandez@gmail.com>
2018-05-13 22:13:19 +0200

    Added simple camera character follower

---
+++ scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject target;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            // camera floating at -8 axis z over the player
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -8);
+            transform.rotation = target.transform.rotation;
+        }
+    }
+}
